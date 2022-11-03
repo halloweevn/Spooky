1523e6766e67d437785eb7bad6d60b5d512b82c7
halloween <107208142+halloweevn@users.noreply.github.com>
2022-11-02 20:03:43 -0700

    Create codes.lua

---
+++ codes.lua
@@ -0,0 +1,18 @@
+local Codes = {
+    "#HAPPYHALLOWEEN!",
+    "WASHINGMACHINE",
+    "BACK2SCHOOL",
+    "ACCOMPLISHMENT",
+    "AUGUST2022!",
+    "DHSUPRISE!",
+    "#FREED",
+    "2022JUNE",
+    "June2022",
+    "FIREWORKS",
+    "freepremiumcrate",
+    "easterdahood",
+    "Stars",
+    "DHUpdate",
+}
+
+return Codes
