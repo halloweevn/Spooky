5b1f00ce5d8a51e4c017ea3ffb2d109dffbbc18a
halloween <107208142+halloweevn@users.noreply.github.com>
2022-12-08 23:14:22 +1100

    Create commands.lua

---
+++ commands.lua
@@ -0,0 +1,14 @@
+.drop --// Loop Drops 7K on Alts
+.stop --// Stops Dropping 7K on Alts
+.setup {admin, bank, school, train, basket}
+.cdrop {amount} --// Drops Until Amount is Reached
+.bring --// Brings Alts to Controller
+.wallet {on/off} --// Equips Alts Wallet
+.freeze {on/off} --// Freezes Alts in Place
+.airlock {on/off} --// Makes Alts Float in Air
+.god --// Makes Alts God Modded
+.advert {on/off} --// Sends Advert Message
+.mask --// Equips Invisble Mask on Alts
+.fps {amount} --// Caps Alts FPS
+.redeem --// Redeems Codes on Alts
+.crash {swag, encrypt, bdh} --// Loads DH Server Crasher
